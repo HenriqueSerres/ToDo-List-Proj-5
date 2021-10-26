8dd08016649ffeaa55885e2a424b4205c747ee7d
Henrique Serres <henrique@gmail.com>
2021-10-26 17:10:55 -0300

    exercicio 5 e 6

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// exercicio 4
 function listaOrdenada() {
   let containerOl = document.querySelector('#container-ol');
   let listaTarefas = document.createElement('ol');
@@ -5,3 +6,15 @@ function listaOrdenada() {
   containerOl.appendChild(listaTarefas);  
 }
 listaOrdenada();
+
+//exercicio 5
+const buttonAdd = document.querySelector('#criar-tarefa');
+const tarefasOl = document.getElementById('lista-tarefas');
+const inputtList = document.getElementById('texto-tarefa');
+buttonAdd.addEventListener('click', function(){
+  let tarefas = inputtList.value;
+  let tarefasLi = document.createElement('li');
+  tarefasLi.innerText = tarefas;
+  inputtList.value = "";
+  tarefasOl.appendChild(tarefasLi);
+})
