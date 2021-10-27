7510b7b3a819bb2b8ed18c6e5e3a5414e4128104
Henrique Serres <henrique@gmail.com>
2021-10-27 16:28:45 -0300

    exercicio 11

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function listaOrdenada() {
   let containerOl = document.querySelector('#container-ol');
   let listaTarefas = document.createElement('ol');
   listaTarefas.id = 'lista-tarefas';
-  containerOl.appendChild(listaTarefas);  
+  containerOl.appendChild(listaTarefas);
 }
 listaOrdenada();
 
@@ -12,7 +12,8 @@ const buttonAdd = document.querySelector('#criar-tarefa');
 const buttonClean = document.getElementById('apaga-tudo');
 const tarefasOl = document.getElementById('lista-tarefas');
 const inputtList = document.getElementById('texto-tarefa');
-buttonAdd.addEventListener('click', function(){
+
+buttonAdd.addEventListener('click', function () {
   let tarefas = inputtList.value;
   let tarefasLi = document.createElement('li');
   tarefasLi.innerText = tarefas;
@@ -21,36 +22,37 @@ buttonAdd.addEventListener('click', function(){
 })
 
 //exercicio 7 e 8
-
 const listaTarefas = document.querySelector('ol');
-function selectLi(event){
+function selectLi(event) {
   let select = document.querySelector('.selected');
   if (select) {
-   select.classList.remove("selected");
+    select.classList.remove("selected");
   }
   event.target.classList.add("selected");
 }
 listaTarefas.addEventListener('click', selectLi);
 
 //exercicio 9
-function riscaLi(event){
-  let riscado = document.querySelector('.completed');
-  if (riscado) {
-   riscado.classList.remove("completed");
-  } else if(!riscado)
-  event.target.classList.add("completed");
+const riscado = document.querySelector('.completed');
+function riscaLi(event) {
+ 
+    event.target.classList.toggle("completed");
 }
 listaTarefas.addEventListener('dblclick', riscaLi);
 
 //exercicio 10
-const takeLi = document.getElementsByTagName('li');
-// buttonClean.addEventListener('click', function(){  
-//   takeLi.value;
-//   listaTarefas.appendChild(takeLi);
-// });
-function cleanTasks(){
-
-    listaTarefas.innerHTML = '';
-  }
-
+function cleanTasks() {
+  listaTarefas.innerHTML = '';
+}
 buttonClean.addEventListener('click', cleanTasks);
+
+//exercicio 11
+const buttonDone = document.getElementById('remover-finalizados')
+function cleanDone() {
+  let riscadoA = document.querySelectorAll('.completed');
+  for (let li of riscadoA){
+    li.remove();
+  }
+//console.log('alo');
+}
+buttonDone.addEventListener('click', cleanDone);
